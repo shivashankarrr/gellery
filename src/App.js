13e0b30371048f2673b5c6affbcb2d2711a9dd10
shiva <shankuu786@gmail.com>
2023-09-20 08:22:54 +0000

    error

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css'
+
+import History from './Components/History'
+
+// These are the list used in the application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => <History />
+
+export default App
---
+++ src/Components/HistoryItem/index.js
@@ -0,0 +1,33 @@
+import './index.css'
+
+const HistoryItem = props => {
+  const {historyList, deleteHistory} = props
+  const {timeAccessed, logoUrl, title, domainUrl, id} = historyList
+
+  const onDelete = () => {
+    deleteHistory(id)
+  }
+
+  return (
+    <li className="list-item">
+      <div className="history-container">
+        <div className="history-details">
+          <p className="time">{timeAccessed}</p>
+          <img src={logoUrl} alt="domain logo" className="icon" />
+          <p className="icon-name">{title}</p>
+          <p className="icon-url"> {domainUrl}</p>
+        </div>
+        <div className="history-button">
+          <button data-testid="delete" type="button" className="button">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+              alt="delete"
+              onClick={onDelete}
+            />
+          </button>
+        </div>
+      </div>
+    </li>
+  )
+}
+export default HistoryItem
